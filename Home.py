980a52d29a798a46a6ad3377622c36e876084256
QamarAyesha <138522264+QamarAyesha@users.noreply.github.com>
2025-03-21 16:11:50 +0500

    Update Home.py

--- Home.py
+++ Home.py
@@ -183,7 +183,7 @@ with col3:
         <div class="key-feature-box">
             <strong>Community Alerts</strong>  
             <ul>
-                <li>Send real-time dengue outbreak alerts to keep communities informed and prepared.</li>
+                <li>Send real-time dengue outbreak alerts to keep communities informed.</li>
                 <li>Engage communities in reporting and prevention efforts.</li>
             </ul>
         </div>
